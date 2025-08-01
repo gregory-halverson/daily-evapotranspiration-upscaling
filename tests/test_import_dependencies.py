958414bb5ad805b83a5b1a411d863a916e9358e6
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-07-31 17:16:24 -0700

    fixing dependency import tests

--- tests/test_import_dependencies.py
+++ tests/test_import_dependencies.py
@@ -2,9 +2,13 @@ import pytest
 
 # List of dependencies
 dependencies = [
+    "dateutil",             # python-dateutil is imported as dateutil
     "numpy",
+    "pandas",
     "rasters",
-    "solar_apparent_time"
+    "solar_apparent_time",  # solar-apparent-time is imported as solar_apparent_time
+    "sun_angles",           # sun-angles is imported as sun_angles
+    "verma_net_radiation"   # verma-net-radiation is imported as verma_net_radiation
 ]
 
 # Generate individual test functions for each dependency
